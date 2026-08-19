685b8f3f324b918859881eb0f52568eb35b9a019
Amardeep Saini <amrdeepsaini@gmail.com>
2026-08-19 20:04:55 +0530

    prod related changes added

--- boards/OPENMV_RT1060/manifest.py
+++ boards/OPENMV_RT1060/manifest.py
@@ -60,11 +60,7 @@ freeze("$(OMV_LIB_DIR)/", "_sx126x.py")
 freeze("$(OMV_LIB_DIR)/", "sx126x.py")
 freeze("$(OMV_LIB_DIR)/", "sx1262.py")
 freeze("$(OMV_LIB_DIR)/", "utils.py")
-freeze("$(OMV_LIB_DIR)/", "wifi_comm.py")
-freeze("$(OMV_LIB_DIR)/", "power_mgmt.py")
-freeze("$(OMV_LIB_DIR)/", "clock_utils.py")
 freeze("$(OMV_LIB_DIR)/", "config_store.py")
-freeze("$(OMV_LIB_DIR)/", "watchdog.py")
 
 # rsa package
 # freeze("$(OMV_LIB_DIR)/rsa")
